c8fa13fa7cbeda1005d10688bedaa97f91d3573e
Al3J077 <garciaalejo711@gmail.com>
2026-02-26 07:43:08 +0000

    login_logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { supabase } from './supabase';
 
-function App() {
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  async function manejarLogin(e) {
+    e.preventDefault();
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) alert(error.message);
+    else alert("¡Bienvenido al sistema!");
+  }
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Iniciar Sesión</h2>
+      <form onSubmit={manejarLogin}>
+        <input
+          type="email"
+          placeholder="Correo electrónico"
+          onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+          type="password"
+          placeholder="Contraseña"
+          onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">ENTRAR</button>
+      </form>
+      </div>
+  );
+
+    }
+  
+
+function Inventario(){
   //Aqui va la LOGICA (CEREBRO)
   const [datos, setDatos] = useState({
     nombre: '',
@@ -41,8 +77,25 @@ function manejarCambio(evento) {
         } 
       }
 
+     async function cerrarSesion() {
+      const { error } = await supabase.auth.signOut();
+
+      if (error) {
+        alert("Error al salir: " + error.message);
+      }
+     }
+
   return (
     <>
+    <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+      <h2>INVENTARIO</h2>
+      <button
+      onClick={cerrarSesion}
+      style={{ background: '#d9534f', color: 'white', border: 'none', padding: '5px 10px', cursor: 'pointer' }}>
+      CERRAR SESIÓN
+      </button>
+    </div>
+
     <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px' }}>
 
       {/*Input de NOMBRE*/}
@@ -73,4 +126,34 @@ function manejarCambio(evento) {
     </>
   );
 }
+
+function App() {
+  // saber si hay alguen logueado
+  const [sesion, setSesion] = useState(null);
+  // 2. useeFFECT es el vigilante q se ejeuta al abrir la app
+  useEffect(() => {
+    // hay una sesion guardada de supabase en el navegador?
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSesion(session);
+    });
+
+    // escuchamos entiepo real si e estado cambia (login o cierre de sesion)
+    const { data: {subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSesion(session);
+    });
+
+    // se limpia el vigilante si se cierra el componente
+    return () => subscription.unsubscribe();
+  }, []);
+
+  // 3. decision del vigilante:
+  if (!sesion) {
+    // si la sesion esta vacia se mostrara el componente login
+    return <Login />;
+  } else {
+    //si la sesion tiene datos, se pasará directamente al inventario
+    return <Inventario />;
+  }
+  }
+
 export default App;
